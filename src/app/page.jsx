dde6a45c81ed04c8fb5aab552bce7a7862fdd0f2
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-08-26 01:33:40 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
-export default function Home(){
-  return(
-    <>
-    Home
-    </>
-  )
+'use client';
+
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+  // Redireciona para /home/dashboard
+  redirect('/home/dashboard');
+  return null;
 }
