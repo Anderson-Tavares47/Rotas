68dae3d172f83614b44f0c235aabf692c550c8d3
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-08-26 01:49:13 -0300

    Update action.jsx

--- src/app/(protected)/home/dashboard/action.jsx
+++ src/app/(protected)/home/dashboard/action.jsx
@@ -1,42 +1,42 @@
-'use server'
+'use server';
 
 import axios from 'axios';
 
 export async function getCoordinates(address) {
-  const response = await axios.get('https://nominatim.openstreetmap.org/search', {
-    params: {
-      q: address,
-      format: 'json',
-      addressdetails: 1,
-      limit: 1,
-    },
-  });
+  try {
+    const response = await axios.get('https://nominatim.openstreetmap.org/search', {
+      params: {
+        q: address,
+        format: 'json',
+        addressdetails: 1,
+        limit: 1,
+      },
+    });
 
-  console.log(response, 'aqui response')
+    console.log(response.data, 'aqui response');
 
-  if (response.data.length === 0) {
-    throw new Error('Address not found');
+    if (response.data.length === 0) {
+      throw new Error('Endereço não encontrado');
+    }
+
+    const { lat, lon } = response.data[0];
+    return [parseFloat(lat), parseFloat(lon)];
+  } catch (error) {
+    console.error('Erro ao obter as coordenadas:', error.message || error);
+    throw new Error('Erro ao obter as coordenadas'); // Rethrow o erro para ser tratado em um nível superior
   }
-
-  const { lat, lon } = response.data[0];
-  return [parseFloat(lat), parseFloat(lon)];
 }
 
 export async function getRoutes(start, end) {
-  const response = await axios.get(`https://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&alternatives=true&geometries=geojson`);
-  const routes = response.data.routes.map(route => ({
-    coordinates: route.geometry.coordinates.map(coord => [coord[1], coord[0]]),
-    duration: route.duration // duração em segundos
-  }));
-  return routes;
+  try {
+    const response = await axios.get(`https://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&alternatives=true&geometries=geojson`);
+    const routes = response.data.routes.map(route => ({
+      coordinates: route.geometry.coordinates.map(coord => [coord[1], coord[0]]),
+      duration: route.duration // duração em segundos
+    }));
+    return routes;
+  } catch (error) {
+    console.error('Erro ao obter as rotas:', error.message || error);
+    throw new Error('Erro ao obter as rotas'); // Rethrow o erro para ser tratado em um nível superior
+  }
 }
-
-
-// import { revalidatePath  } from "next/cache"
-// import * as apis from '@/core/clients/drivers'
-
-// export async function signin() {
-//   revalidatePath('/teste/dashboard')
-//   const response = await apis.teste()
-//   return response
-// }
